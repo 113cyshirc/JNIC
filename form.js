54b2a637c7c19573c5b5354e89ea6efcd1134266
113cyshirc <cyshirc@gmail.com>
2024-06-12 22:19:31 +0800

    修正iframe+新增開頭動畫

--- form.js
+++ form.js
@@ -1,5 +1,7 @@
 function show_form() {
-    $("#sign-up.form")[0].style.display = "block";
-    window.open("./index.html#sign_up","_self");
+    $("#sign-up.form").fadeIn('fast');
 }
 
+function hide_form() {
+    $("#sign-up.form").fadeOut('fast');
+}
