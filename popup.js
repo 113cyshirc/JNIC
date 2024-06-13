2055c6a4f8d62b17086c9bd6cd7f95eaf426f812
113cyshirc <cyshirc@gmail.com>
2024-06-13 11:47:57 +0800

    新增平面圖

--- popup.js
+++ popup.js
@@ -5,3 +5,12 @@ function show_form() {
 function hide_form() {
     $("#sign-up.form").fadeOut('fast');
 }
+
+
+function show_map() {
+    $("#map.map").fadeIn('fast');
+}
+
+function hide_map() {
+    $("#map.map").fadeOut('fast');
+}
