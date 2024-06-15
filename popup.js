4ca3e3a262f09ca579269a53b5432e0a1899e9d3
KnowScratcher <yianlee2008@gmail.com>
2024-06-15 21:06:22 +0800

    新增交通資訊

--- popup.js
+++ popup.js
@@ -60,10 +60,44 @@ function hide_map() {
     $("#map.map").fadeOut('fast');
 }
 
-
-
-window.onclick = function(event) {
-    if (event.target == $("#map.map")[0]) {
-        $("#map.map").fadeOut('fast')
-    }
-  }
+$(document).ready(function () {
+    $("#map.map:not(.map-content)").click(function(e) {
+        var container = $(".map-content");
+        if (!container.is(e.target) && container.has(e.target).length === 0) {
+            $("#map.map").fadeOut('fast');
+        }
+    });
+    $("#sign-up.form").click(function() {
+        hide_form();
+    });
+    $(".s_button").click(function() {
+        $(".selected").attr("class","s_button");
+        $(this).attr("class","s_button selected");
+        $(".show").slideUp(function() {
+            $(this).attr("class","plate show");
+        });
+        var id = $(this).attr("id");
+        switch(id){
+            case "bus_r":
+                $("#bus_r_plate").slideDown(function() {
+                    $(this).attr("class","plate show");
+                    window.location.replace("#select_route");
+                });
+                break;
+            case "train_n":
+                $("#train_n_plate").slideDown(function() {
+                    $(this).attr("class","plate show");
+                    window.location.replace("#select_route");
+                });
+                break;
+            case "train_s":
+                $("#train_s_plate").slideDown(function() {
+                    $(this).attr("class","plate show");
+                    window.location.replace("#select_route");
+                });
+                break;
+            default:
+                window.location.replace("#route");
+        }
+    });
+});
